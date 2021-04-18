a02cd2d79df7a55ccefb90c5304383c0175a2c7a
Anthonini <anthonini@gmail.com>
2021-04-18 17:43:39 -0300

    Configurado conexão com o banco e criação de usuário.
    Signed-off-by: Anthonini <anthonini@gmail.com>

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb://aircnc:aircnc@ancluster-shard-00-00.nnapr.mongodb.net:27017,ancluster-shard-00-01.nnapr.mongodb.net:27017,ancluster-shard-00-02.nnapr.mongodb.net:27017/aircnc?ssl=true&replicaSet=atlas-fpp63a-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(routes);
