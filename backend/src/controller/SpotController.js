c250a84d6e6cd67ff1565fb6bfd87314e3ba8bbd
Anthonini <anthonini@gmail.com>
2021-04-18 19:45:40 -0300

    Criado a rota de listagens de spots.
    Signed-off-by: Anthonini <anthonini@gmail.com>

--- backend/src/controller/SpotController.js
+++ backend/src/controller/SpotController.js
@@ -2,6 +2,14 @@ const Spot = require('../model/Spot');
 const User = require('../model/User');
 
 module.exports = {
+    async index(req, res) {
+        const {tech} = req.query;
+
+        const spots = await Spot.find({ techs: tech});
+
+        return res.json({ spots });
+    },
+
     async store(req, res) {
         const { filename } = req.file;
         const {company, techs, price} = req.body;
